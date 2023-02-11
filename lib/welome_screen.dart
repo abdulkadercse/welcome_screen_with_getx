357e2a737575f5ae4efbd5dc8e6686d63f346f02
abdulkadercse <kaderhosen2002@gmail.com>
2023-02-11 11:05:22 +0600

    First

--- lib/welome_screen.dart
+++ lib/welome_screen.dart
@@ -45,86 +45,89 @@ class WelcomeScreen extends StatelessWidget {
           Expanded(
             flex: 1,
             child: Center(
-              child: Container(
-                width: 364,
-                height: 314,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Color(0xffE9EDF0),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.black54,
-                        blurRadius: 15.0,
-                        offset: Offset(0.0, 0.75),
-                      )
-                    ]),
-                child: Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Obx(
-                        () => Text(
-                          title[_curentIndex.toInt()],
-                          style: const TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w700,
-                              color: Colors.black),
-                        ),
-                      ),
-                      Obx(
-                        () => Text(
-                          description[_curentIndex.toInt()],
-                          style: const TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w700,
-                              color: Colors.black),
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Obx(
-                            () => DotsIndicator(
-                              dotsCount: title.length,
-                              position: _curentIndex.toDouble(),
-                            ),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  width: 364,
+                  height: 314,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color(0xffE9EDF0),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black54,
+                          blurRadius: 15.0,
+                          offset: Offset(0.0, 0.75),
+                        )
+                      ]),
+                  child: Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Obx(
+                          () => Text(
+                            title[_curentIndex.toInt()],
+                            style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w700,
+                                color: Colors.black),
                           ),
-                          InkWell(
-                            onTap: () {
-                              if (_curentIndex == 2) {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (contex) => HomeScreen()));
-                              } else {
-                                _curentIndex + 1;
-                              }
-                            },
-                            child: Container(
-                              height: 37,
-                              width: 37,
-                              decoration: const BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Color(0xffE9EDF0),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.black54,
-                                      blurRadius: 5.0,
-                                      offset: Offset(0.0, 0.75),
-                                    )
-                                  ]
-                              ),
-                              child: Icon(
-                                Icons.arrow_forward_ios,
-                                size: 15,
+                        ),
+                        Obx(
+                          () => Text(
+                            description[_curentIndex.toInt()],
+                            style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w700,
+                                color: Colors.black),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Obx(
+                              () => DotsIndicator(
+                                dotsCount: title.length,
+                                position: _curentIndex.toDouble(),
                               ),
                             ),
-                          )
-                        ],
-                      )
-                    ],
+                            InkWell(
+                              onTap: () {
+                                if (_curentIndex == 2) {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (contex) => HomeScreen()));
+                                } else {
+                                  _curentIndex + 1;
+                                }
+                              },
+                              child: Container(
+                                height: 37,
+                                width: 37,
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Color(0xffE9EDF0),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.black54,
+                                        blurRadius: 5.0,
+                                        offset: Offset(0.0, 0.75),
+                                      )
+                                    ]
+                                ),
+                                child: Icon(
+                                  Icons.arrow_forward_ios,
+                                  size: 15,
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
